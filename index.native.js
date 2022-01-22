a5f3642b955ad2638394b7f07d405a49ef686ce6
Chris Griffing <cmgriffing@gmail.com>
2022-01-22 13:38:09 -0800

    fix: invert y for swipe method

--- index.native.js
+++ index.native.js
@@ -124,9 +124,9 @@ const TinderCard = React.forwardRef(
         } else if (dir === "left") {
           await animateOut({ x: -power, y: disturbance }, setSpringTarget);
         } else if (dir === "up") {
-          await animateOut({ x: disturbance, y: power }, setSpringTarget);
-        } else if (dir === "down") {
           await animateOut({ x: disturbance, y: -power }, setSpringTarget);
+        } else if (dir === "down") {
+          await animateOut({ x: disturbance, y: power }, setSpringTarget);
         }
         if (onCardLeftScreen) onCardLeftScreen(dir);
       },
